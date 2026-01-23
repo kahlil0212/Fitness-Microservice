9dc9503ceffd3fd2bfa18224c5b38a1e614e2e01
kahlil <kahlil0212@gmail.com>
2026-01-23 16:32:08 -0500

    added modelmapper config to user service. refactored mapping to use modelmapper instead of repeated setters

--- user-service/src/main/java/com/fitness/user_service/service/UserService.java
+++ user-service/src/main/java/com/fitness/user_service/service/UserService.java
@@ -6,13 +6,19 @@ import com.fitness.user_service.model.User;
 import com.fitness.user_service.repository.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     public UserResponse register(@Valid RegisterRequest request) {
 
@@ -20,45 +26,19 @@ public class UserService {
             throw new RuntimeException("Email already exists");
         }
 
-        User newUser = new User();
-
-        //TODO: Use ModelMapper pattern
-        newUser.setEmail(request.getEmail());
-        newUser.setPassword(request.getPassword());
-        newUser.setFirstName(request.getFirstName());
-        newUser.setLastName(request.getLastName());
+        User newUser = modelMapper.map(request,User.class);
 
         User savedUser = userRepository.save(newUser);
 
-        UserResponse userResponse = new UserResponse();
-
-        //TODO: Use ModelMapper pattern
-        userResponse.setId(savedUser.getId());
-        userResponse.setEmail(savedUser.getEmail());
-        userResponse.setPassword(savedUser.getPassword());
-        userResponse.setFirstName(savedUser.getFirstName());
-        userResponse.setLastName(savedUser.getLastName());
-        userResponse.setCreatedDate(savedUser.getCreatedDate());
-        userResponse.setUpdatedDate(savedUser.getUpdatedDate());
-
-        return userResponse;
+        return modelMapper.map(savedUser,UserResponse.class);
     }
 
     public UserResponse getUserProfile(String userId) {
 
         User retrievedUser = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
 
-        //TODO: Use ModelMapper pattern
-        UserResponse userResponse = new UserResponse();
-        userResponse.setId(retrievedUser.getId());
-        userResponse.setEmail(retrievedUser.getEmail());
-        userResponse.setPassword(retrievedUser.getPassword());
-        userResponse.setFirstName(retrievedUser.getFirstName());
-        userResponse.setLastName(retrievedUser.getLastName());
-        userResponse.setCreatedDate(retrievedUser.getCreatedDate());
-        userResponse.setUpdatedDate(retrievedUser.getUpdatedDate());
 
-        return userResponse;
+        return modelMapper.map(retrievedUser,UserResponse.class);
     }
 
     public Boolean existsByUserId(String userId) {
