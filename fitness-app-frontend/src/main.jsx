8fded38d6b9ec6417ee282c7e6bd2bb698852b66
kahlil <kahlil0212@gmail.com>
2026-02-01 16:40:05 -0500

    fixing issue with slash in redirect ui causing issue. adding logic to set user auth state details after login

--- fitness-app-frontend/src/main.jsx
+++ fitness-app-frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.jsx'
+import { AuthProvider } from 'react-oauth2-code-pkce'
+import { authConfig } from './authentication/authConfig.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+  <AuthProvider authConfig={authConfig}>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>,
+  </AuthProvider>,
 )
--- fitness-app-frontend/src/store/authSlice.jsx
+++ fitness-app-frontend/src/store/authSlice.jsx
@@ -8,16 +8,30 @@ export const authSlice = createSlice({
     userId: localStorage.getItem('userId') || null
   },
   reducers: {
-   setCredentials: (state, action) => {
+    setCredentials: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.userId = action.payload.user.sub;
 
-   },
-   logout: (state) => {
+      localStorage.setItem('token', action.payload.token);
+      localStorage.setItem('user', JSON.stringify(action.payload.user));
+      localStorage.setItem('userId', action.payload.user.sub);
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      state.userId = null;
 
-   }
+      localStorage.removeItemItem('token');
+      localStorage.removeItemItem('user');
+      localStorage.removeItemItem('userId');
+
+
+    }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const {setCredentials,logout } = authSlice.actions;
+export const { setCredentials, logout } = authSlice.actions;
 
 export default authSlice.reducer;
