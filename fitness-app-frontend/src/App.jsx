4fd85204d7256a18e839be1df11b3540d4e3cead
kahlil <kahlil0212@gmail.com>
2026-02-01 15:28:40 -0500

    integrating with keycloak and adding oauth2 pkce library

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,13 +1,14 @@
 
-import './App.css'
+import { Button } from "@mui/material"
+import { BrowserRouter as Router } from "react-router"
 
 function App() {
-  
+
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Button variant="contained" color="primary">Login</Button>
+    </Router>
   )
 }
 
